5db1c26b6fc515c2ffbba52b3ab1bc3cda61e8aa
mr.jogurt <mr.jogurt@zotts.de>
2022-02-13 18:55:13 +0100

    (#18) OpenAdventureException with StackTrace message creator. * All Custom exceptions should extend from OpenAdventureException

---
+++ jvGuiApplication/src/main/java/mj/openadventure/exceptions/OpenAdventureException.java
@@ -0,0 +1,39 @@
+package mj.openadventure.exceptions;
+
+import static mj.openadventure.exceptions.OpenAdventureStackTrace.getCustomExceptionMessageFromStackTrace;
+
+/**
+ * Parent-class for all custom Exception classes
+ *
+ * @author corb
+ */
+public class OpenAdventureException extends Exception{
+
+    /**
+     * Constructor for StackTrace as message.
+     *
+     * @param e Exception that was thrown.
+     */
+    public OpenAdventureException(Exception e){
+        super(getCustomExceptionMessageFromStackTrace(e));
+    }
+
+    /**
+     * Constructor for String as message.
+     *
+     * @param msg String message that is defined when thrown.
+     */
+    public OpenAdventureException(String msg){
+        super(msg);
+    }
+
+    /**
+     * Constructor for String and StackTrace as message.
+     *
+     * @param msg String message that is defined when thrown.
+     * @param e Exception that was thrown.
+     */
+    public OpenAdventureException(String msg, Exception e){
+        super(msg + "\r\n" + getCustomExceptionMessageFromStackTrace(e));
+    }
+}
