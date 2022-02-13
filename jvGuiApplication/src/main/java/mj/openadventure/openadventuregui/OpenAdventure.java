a45830d7c3630378c17c5669adba63cbba7891bc
mr.jogurt <mr.jogurt@zotts.de>
2022-02-13 16:20:30 +0100

    (#16) Config file plus Parser class.
    * cfg.xml file as first config file. More are addable easily.
    * Parser file takes key and loads config nodes.
    * RuntimeVars contains global static runtime variables.
    * ConfigFiles contains enums for every config file.

--- jvGuiApplication/src/main/java/mj/openadventure/openadventuregui/OpenAdventure.java
+++ jvGuiApplication/src/main/java/mj/openadventure/openadventuregui/OpenAdventure.java
@@ -1,8 +1,19 @@
 package mj.openadventure.openadventuregui;
 
+import mj.openadventure.exceptions.ConfigParsingException;
+import mj.openadventure.globals.RuntimeVars;
+import mj.openadventure.io.ConfigParser;
+
 public class OpenAdventure {
 
     public static void main(String[] args) {
-
+        RuntimeVars rv = new RuntimeVars(System.getProperty("user.dir") + "/src/main");
+        try {
+            new ConfigParser().loadConfigFile();
+            System.out.println(new ConfigParser().getConfigNode("testNode"));
+        } catch (ConfigParsingException e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
     }
 }
